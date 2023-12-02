0952d4c2f30fc8464fb91e2e8b9e6b6b3f582ea7
DinuD <dragomirescudinu@gmail.com>
2023-12-02 11:24:47 +0000

    Improve start day script

--- start-day.sh
+++ start-day.sh
@@ -3,6 +3,9 @@ if [ $# -eq 1 ]
 then
     cp -R template/ Day$1/
     mv Day$1/Day00.ts Day$1/Day$1.ts
+    touch Day$1/input.txt
+    touch Day$1/example.txt
+    echo "Day $1 created"
 else
     echo "You need to specify a day number"
 fi
